ef1c3c88f220f0f6af040898b88b0e50000a1485
Chandra Delon <34028244+vinix997@users.noreply.github.com>
2019-05-08 14:03:51 +0700

    Fix and Update
    ON/OFF and Volume Slider works perfectly
    Game Over Screen Added

--- Assets/Script/Toogler.cs
+++ Assets/Script/Toogler.cs
@@ -3,18 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 public class Toogler : MonoBehaviour {
-
+	public Toggle onSwitch;
+	public void Start()
+	{
+		LoadSetting();		
+	}
 	public void Switching()
 	{
-		bool onSwitch = gameObject.GetComponent<Toggle>().isOn;
-		if(onSwitch)
+		if(onSwitch.isOn)
 		{
 			Debug.Log("switch is on");
 				AudioListener.pause = false;
+			PlayerPrefs.SetInt("switch",1);
 		}
 		else
 		{
 			AudioListener.pause = true;
+			PlayerPrefs.SetInt("switch",0);
+		}
+	}
+	// public void SaveSetting()
+	// {
+	// 	PlayerPrefs.SetInt("switch",onSwitch ? 1 : 0);
+	// 	PlayerPrefs.Save();
+	// }
+	public void LoadSetting()
+	{
+		if(PlayerPrefs.GetInt("switch")==1)
+		{
+			onSwitch.isOn=true;
+		}		
+		else{
+			onSwitch.isOn=false;
 		}
 	}
 }
--- Assets/Script/VolumeSlider.cs
+++ Assets/Script/VolumeSlider.cs
@@ -2,14 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 public class VolumeSlider : MonoBehaviour {
 	public AudioMixer mainMenu;
 	public AudioMixer ingame;
     public float save;
-   
+    public Slider vol;
+    public void Start()
+    {
+       save = PlayerPrefs.GetFloat("saved");
+       vol.value = save;
+    }
 	public void SetVolume(float volume)
     {
         mainMenu.SetFloat("volume", volume);
+        save = volume;
+        PlayerPrefs.SetFloat("saved",save);
+        PlayerPrefs.Save();
+        
     }
+  
+    
 
 }
