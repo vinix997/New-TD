07cd4cfabe7656e6e446ca74a8aca76a16be5504
Crouston <yosuam34@gmail.com>
2019-05-11 03:24:05 +0700

    pressed added

--- Assets/Script/ChoosingButton.cs
+++ Assets/Script/ChoosingButton.cs
@@ -6,7 +6,22 @@ public class ChoosingButton : MonoBehaviour {
 
     [SerializeField] private GameObject wizardSelected;
 
-	public void ChooseWizard()
+    [SerializeField]
+    private Sprite pressed,idle;
+
+    private void Update()
+    {
+        if(FindObjectOfType<GameManager>().selectedWizard == wizardSelected)
+        {
+            GetComponent<SpriteRenderer>().sprite = pressed;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().sprite = idle;
+        }
+    }
+
+    public void ChooseWizard()
     {
         GameManager.FindObjectOfType<GameManager>().selectedWizard = wizardSelected;
     }
